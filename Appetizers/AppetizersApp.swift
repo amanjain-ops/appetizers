26b1b621c784bfa56ed3384f0ab9d95b30e76bf6
amanjain-ops <jainaman06060@gmail.com>
2024-09-19 10:44:38 +0530

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Aman Jain on 19/09/24.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
