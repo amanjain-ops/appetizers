c4e6226f55583244d7eabc29114172a112af0951
amanjain-ops <jainaman06060@gmail.com>
2024-09-26 12:29:23 +0530

    add image and placeholder

--- Appetizers/AppetizerListCell.swift
+++ Appetizers/AppetizerListCell.swift
@@ -11,12 +11,20 @@ struct AppetizerListCell: View {
     let appetizer: Appetizer
     var body: some View {
         HStack {
-            Image("asian-flask-steak")
-                .resizable()
-                .scaledToFit()
-                .frame(width: 120, height: 90)
-                .clipShape(RoundedRectangle(cornerRadius: 8))
-                
+            AsyncImage(url: URL(string: appetizer.imageURL)) { image in
+                image
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 120, height: 90)
+                    .clipShape(RoundedRectangle(cornerRadius: 8))
+                    
+            } placeholder: {
+                Image("food-placeholder")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 120, height: 90)
+                    .clipShape(RoundedRectangle(cornerRadius: 8))
+            }
             
             VStack(alignment: .leading, spacing: 5) {
                 Text(appetizer.name)
