1ed27544934db81cd6c2d538165001feb1940635
amanjain-ops <jainaman06060@gmail.com>
2024-09-28 11:24:31 +0530

    Create Account page

--- Appetizers/Screens/AccountView/AccountView.swift
+++ Appetizers/Screens/AccountView/AccountView.swift
@@ -8,10 +8,46 @@
 import SwiftUI
 
 struct AccountView: View {
+    @State private var firstName: String = ""
+    @State private var lasttName: String = ""
+    @State private var email: String = ""
+    @State private var birthdate = Date()
+    @State private var extraNapkins = false
+    @State private var frequentRefills = false
+    
     var body: some View {
         NavigationStack {
-            Text("Account View")
+            VStack(alignment: .leading) {
+                Form {
+                    Section("Personal Info"){
+                        TextField("First Name", text: $firstName)
+                        TextField("Last Name", text: $lasttName)
+                        TextField("Email", text: $email)
+                            .keyboardType(.emailAddress)
+                            .textInputAutocapitalization(.none)
+                            .autocorrectionDisabled(true)
+                        
+                        DatePicker("Birthday", selection: $birthdate, displayedComponents: .date)
+                        
+                        
+                        Button {
+                            print("Save")
+                        } label: {
+                            Text("Save Changes")
+                            //                            .foregroundStyle(Color(.brandPrimary))
+                        }
+                    } // personal info section
+                    
+                    Section("Requests") {
+                        Toggle("Extra Napkins", isOn: $extraNapkins)
+                        
+                        Toggle("Frequent Refills", isOn: $frequentRefills)
+                    }
+                    .toggleStyle(SwitchToggleStyle(tint: .brandPrimaryy))
+                    
+                }
                 .navigationTitle("🤣 Account")
+            }
         }
     }
 }
