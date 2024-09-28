a415b262aa95c62c57f68923888221e2693c1ce8
amanjain-ops <jainaman06060@gmail.com>
2024-09-28 11:46:36 +0530

    Add Alerts for Account
    Add Alerts for Account

---
+++ Appetizers/Screens/AccountView/AccountViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AccountViewModel.swift
+//  Appetizers
+//
+//  Created by Aman Jain on 28/09/24.
+//
+
+import Foundation
---
+++ Appetizers/Utilities/Extenstions/String+Ext.swift
@@ -0,0 +1,8 @@
+//
+//  String+Ext.swift
+//  Appetizers
+//
+//  Created by Aman Jain on 28/09/24.
+//
+
+import Foundation
