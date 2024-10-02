31ed29776fbe735ea3257d2e68573e5916529926
amanjain-ops <jainaman06060@gmail.com>
2024-10-02 11:17:49 +0530

    Mock data added for order

--- Appetizers/Model/Appetizer.swift
+++ Appetizers/Model/Appetizer.swift
@@ -36,4 +36,35 @@ struct MockData {
                                            carbs: 99)
     
     static let appetizers = [sampleAppetizer, sampleAppetizer, sampleAppetizer, sampleAppetizer]
+    
+    
+    static let orderItemOne = Appetizer(id: 0001,
+                                           name: "Test Appetizer One",
+                                           description: "This is description for my appetizer. It's yummy.",
+                                           price: 9.99,
+                                           imageURL: "https://seanallen-course-backend.herokuapp.com/images/appetizers/asian-flank-steak.jpg",
+                                           calories: 99,
+                                           protein: 99,
+                                           carbs: 99)
+    
+    static let orderItemTwo = Appetizer(id: 0002,
+                                           name: "Test Appetizer Two",
+                                           description: "This is description for my appetizer. It's yummy.",
+                                           price: 9.99,
+                                           imageURL: "https://seanallen-course-backend.herokuapp.com/images/appetizers/asian-flank-steak.jpg",
+                                           calories: 99,
+                                           protein: 99,
+                                           carbs: 99)
+    
+    
+    static let orderItemThree = Appetizer(id: 0003,
+                                           name: "Test Appetizer Three",
+                                           description: "This is description for my appetizer. It's yummy.",
+                                           price: 9.99,
+                                           imageURL: "https://seanallen-course-backend.herokuapp.com/images/appetizers/asian-flank-steak.jpg",
+                                           calories: 99,
+                                           protein: 99,
+                                           carbs: 99)
+    
+    static let orderItem = [orderItemOne, orderItemTwo, orderItemThree]
 }
