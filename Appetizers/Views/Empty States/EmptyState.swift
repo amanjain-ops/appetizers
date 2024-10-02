a262df928ff43dde4f806f9811971c562e46a4fc
amanjain-ops <jainaman06060@gmail.com>
2024-10-02 11:40:20 +0530

    Custom Empty state created

---
+++ Appetizers/Views/Empty States/EmptyState.swift
@@ -0,0 +1,38 @@
+//
+//  EmptyState.swift
+//  Appetizers
+//
+//  Created by Aman Jain on 02/10/24.
+//
+
+import SwiftUI
+
+struct EmptyState: View {
+    let imageName: String
+    let message: String
+    var body: some View {
+        ZStack{
+            Color(.systemBackground)
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Image(imageName)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 150)
+                
+                Text(message)
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                    .multilineTextAlignment(.center)
+                    .foregroundStyle(.secondary)
+                    .padding()
+            }
+        }
+        
+    }
+}
+
+#Preview {
+    EmptyState(imageName: "empty-order", message: "This is our text message, i'm making it long for testing")
+}
