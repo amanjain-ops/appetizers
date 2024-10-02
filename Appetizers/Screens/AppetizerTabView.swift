4ac0bc897cd012dff6cf6ff6ec0ada3b8111cabe
amanjain-ops <jainaman06060@gmail.com>
2024-10-02 20:06:26 +0530

    .

--- Appetizers/Screens/AppetizerTabView.swift
+++ Appetizers/Screens/AppetizerTabView.swift
@@ -26,7 +26,7 @@ struct AppetizerTabView: View {
                     Text("Order")
                 }
         }
-        .accentColor(.brandPrimaryy)
+//        .accentColor(.brandPrimaryy)
     }
 }
 
